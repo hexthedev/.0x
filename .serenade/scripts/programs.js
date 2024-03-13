cf4d067e0ef18eb7694b034279efe0154448e6e7
James McCafferty <jamesmccafferty@Jamess-MacBook-Pro.local>
2024-03-13 19:56:22 -0400

    changed layout

--- .serenade/scripts/programs.js
+++
@@ -1,7 +0,0 @@
-serenade.global().command("launch chrome work", async api => {
-  await api.runShell('"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"', ['https://www.perplexity.ai/ --profile-directory="Profile 1"'], {shell: true})
-})
-
-serenade.global().command("launch chrome me", async api => {
-  await api.runShell('"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"', ['https://www.perplexity.ai/ --profile-directory="Profile 2"'], {shell: true})
-})
