0e2486e05db2a372a69a4308036c0e136ca0d259
James McCafferty <james.mccafferty@unity3d.com>
2024-04-05 10:33:30 -0400

    Updated script to pull shortcuts too

--- hex.sh
+++ hex.sh
@@ -2,21 +2,34 @@
 
 hexDir=~/repos/.0x
 hexLayoutDir=$hexDir/unity/layouts
+hexShortcutsDir=$hexDir/unity/shortcuts
+
 unityLayoutDir=~/Library/Preferences/Unity/Editor-5.x/Layouts/default
+unityShortcutsDir=~/Library/Preferences/Unity/Editor-5.x/shortcuts/default
+
+performUpdate() {
+  what=$1
+  from=$2
+  to=$3
+
+  echo "Updating $what by copying from $from to $to" 
+  rm -r $to
+  cp -r $from $to
+}
 
 update() {
-  targ=$1
-  dest=$2
+  layoutFrom=$1
+  layoutTo=$2
+  shortcutFrom=$3
+  shortcutTo=$4
 
-  # Unity Layouts
-  echo "Copying Unity Layouts from $targ to $dest" 
-  rm -r $dest
-  cp -r $targ $dest
+  performUpdate "Unity Layouts" $layoutFrom $layoutTo
+  performUpdate "Unity Shortcuts" $shortcutFrom $shortcutTo
 }
 
 case "$1" in
   --pull)
-    update $unityLayoutDir $hexLayoutDir
+    update $unityLayoutDir $hexLayoutDir $unityShortcutsDir $hexShortcutsDir
     ;;
   --push)
     update push
