139ae5af7cb78aab3609ecfdac1bcc4402057c80
James McCafferty <james.mccafferty@unity3d.com>
2024-04-05 10:53:40 -0400

    Updated .ideavimrc push/pull

--- hex.sh
+++ hex.sh
@@ -3,9 +3,12 @@
 hexDir=~/repos/.0x
 hexLayoutDir=$hexDir/unity/layouts
 hexShortcutsDir=$hexDir/unity/shortcuts
+hexIdeaVimRCPath=$hexDir/jetbrains/.ideavimrc
 
 unityLayoutDir=~/Library/Preferences/Unity/Editor-5.x/Layouts/default
 unityShortcutsDir=~/Library/Preferences/Unity/Editor-5.x/shortcuts/default
+
+systemIdeaVimRCPath=~/.ideavimrc
 
 performUpdate() {
   what=$1
@@ -22,17 +25,20 @@ update() {
   layoutTo=$2
   shortcutFrom=$3
   shortcutTo=$4
+  ideaVimRCFrom=$5
+  ideaVimRCTo=$6
 
   performUpdate "Unity Layouts" $layoutFrom $layoutTo
   performUpdate "Unity Shortcuts" $shortcutFrom $shortcutTo
+  performUpdate ".ideavimrc" $ideaVimRCFrom $ideaVimRCTo
 }
 
 case "$1" in
   --pull)
-    update $unityLayoutDir $hexLayoutDir $unityShortcutsDir $hexShortcutsDir
+    update $unityLayoutDir $hexLayoutDir $unityShortcutsDir $hexShortcutsDir $systemIdeaVimRCPath $hexIdeaVimRCPath
     ;;
   --push)
-    update $hexLayoutDir $unityLayoutDir $hexShortcutsDir $unityShortcutsDir
+    update $hexLayoutDir $unityLayoutDir $hexShortcutsDir $unityShortcutsDir $hexIdeaVimRCPath $systemIdeaVimRCPath
     ;;
   *)
     echo "Failed"
