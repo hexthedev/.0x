7e808dc2ec1edd6fb5fc6f8a328b4f8d6e381334
hex <jamesmccafferty@live.ca>
2024-08-31 15:55:57 -0400

    updates

--- hex.sh
+++ hex.sh
@@ -1,19 +1,20 @@
 #!/bin/zsh
 
-hexDir=~/repos/.0x
-hexLayoutDir=$hexDir/unity/layouts
-hexShortcutsDir=$hexDir/unity/shortcuts
-hexJetbrainsKeymapsDir=$hexDir/jetbrains/keymaps
-hexZshRcPath=$hexDir/.zshrc
+# source
+root=~/repos/.0x
+layouts=$root/unity/layouts
+shortcuts=$root/unity/shortcuts
+jb_keys=$root/jetbrains/keymaps
+vs_keys=$root/vscode/keybindings.json
+rc=$root/.zshrc
 
-unityLayoutDir=~/Library/Preferences/Unity/Editor-5.x/Layouts/default
-unityShortcutsDir=~/Library/Preferences/Unity/Editor-5.x/shortcuts/default
-
-systemIdeaVimRCPath=~/.ideavimrc
-systemZshRcPath=~/.zshrc
-
-jetbrainsRiderKeys='/Users/james.mccafferty/Library/Application Support/JetBrains/Rider2024.1/keymaps'
-jetbrainsPycharmKeys='/Users/james.mccafferty/Library/Application Support/JetBrains/PyCharm2024.1/keymaps'
+# dest
+sys_layouts=~/Library/Preferences/Unity/Editor-5.x/Layouts/default
+sys_shortcuts=~/Library/Preferences/Unity/Editor-5.x/shortcuts/default
+sys_rider_keys=~/Library/Application\ Support/JetBrains/Rider2024.1/keymaps
+sys_pycharm_keys=~/Library/Application\ Support/JetBrains/PyCharm2024.1/keymaps
+sys_vs_keys=~/Library/Application\ Support/Code/User/keybindings.json
+sys_rc=~/.zshrc
 
 performUpdate() {
   what=$1
@@ -27,17 +28,19 @@ performUpdate() {
 
 case "$1" in
   --pull)
-    performUpdate "Unity Layouts" $unityLayoutDir $hexLayoutDir
-    performUpdate "Unity Shortcuts" $unityShortcutsDir $hexShortcutsDir
-    performUpdate "Jetbrains Keymaps" $jetbrainsRiderKeys $hexJetbrainsKeymapsDir
-    performUpdate ".zshrc" $systemZshRcPath $hexZshRcPath
+    performUpdate "Unity Layouts" $sys_layouts $layouts
+    performUpdate "Unity Shortcuts" $sys_shortcuts $shortcuts
+    performUpdate "Jetbrains Keymaps" $sys_rider_keys $jb_keys
+    performUpdate "VS Code Keybindings" $sys_vs_keys $vs_keys
+    performUpdate ".zshrc" $sys_rc $rc
     ;;
   --push)
-    performUpdate "Unity Layouts" $hexLayoutDir $unityLayoutDir
-    performUpdate "Unity Shortcuts" $hexShortcutsDir $unityShortcutsDir
-    performUpdate "Jetbrains Keymaps (Rider)" $hexJetbrainsKeymapsDir $jetbrainsRiderKeys
-    performUpdate "Jetbrains Keymaps (Pycharm)" $hexJetbrainsKeymapsDir $jetbrainsPycharmKeys
-    performUpdate ".zshrc" $hexZshRcPath $systemZshRcPath
+    performUpdate "Unity Layouts" $layouts $sys_layouts
+    performUpdate "Unity Shortcuts" $shortcuts $sys_shortcuts
+    performUpdate "Jetbrains Keymaps (Rider)" $jb_keys $sys_rider_keys
+    performUpdate "Jetbrains Keymaps (Pycharm)" $jb_keys $sys_pycharm_keys
+    performUpdate "VS Code Keybindings" $vs_keys $sys_vs_keys
+    performUpdate ".zshrc" $rc $sys_rc
     ;;
   *)
     echo "Failed"
